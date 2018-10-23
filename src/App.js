bc11120945389e987b37b4cb204a91cd6e7f97c8
djferraro <djferraro@gmail.com>
2018-10-23 13:56:30 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dominic from './images/dominic.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="intro">
+              <div className="col">
+                  <div className="profile-pic">
+                    <img src={dominic} alt="Dominic Ferraro"/>
+                  </div>
+              </div>
+              <div className="col">
+                  <div className="intro-content col m10 s12">
+                      <h1>Dominic Ferraro</h1>
+                      <h2>Front-End Developer: vArmour</h2>
+                      <h2 className="small">San Francisco, CA</h2>
+                      <p>My career and business experience has cultivated a unique combination of strength in visual design, 
+                      marketing forte, and aptitude for business operations. I have a business brain painted in creative colors that 
+                      allows me to help organizations drive funnel engagement by understanding business concerns, and using that 
+                      understanding to design and build creative content. </p>
+                      <a href="https://www.linkedin.com/in/dominic-ferraro-6b54163/" className="btn btn-success">Linked<i class="fab fa-linkedin"></i></a>
+                  </div>
+              </div>
+          </div>
+        </div> 
       </div>
     );
   }
